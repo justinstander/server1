c755760d95caf368b7469841015856bb408eeba7
justinstander <justin@sundaygoods.com>
2020-03-08 17:10:30 -0400

    oh my glob

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd "./src/$1" && zip -r "$1.zip" *.js && aws lambda update-function-code --function-name $1 --zip-file "fileb://$1.zip" && rm "$1.zip"
+cd "./src/$1" && zip -r "$1.zip" ./*.js && aws lambda update-function-code --function-name $1 --zip-file "fileb://$1.zip" && rm "$1.zip"
