493c83ef421bf61fd4cf02d57acc5dd79c3b9838
justinstander <justin@sundaygoods.com>
2020-03-08 16:55:41 -0400

    Modularized putTotalCost

--- src/haasandmilan-api-putTotalCost/index.js
+++ src/haasandmilan-api-putTotalCost/index.js
@@ -1,68 +1,7 @@
-// DynamoDB
-const DynamoDB = require("aws-sdk/clients/dynamodb");
-const dynamodb = new DynamoDB();
 
-const TableName = "CostAndUsage";
-
-const clearItems = async () => {
-  const { Items } = await dynamodb.scan({
-    TableName
-  }).promise();
-  
-  while (Items.length > 0) {
-    await dynamodb.deleteItem({
-      TableName,
-      Key: {
-        AwsRequestId: {
-          S: Items.pop().AwsRequestId.S
-        }
-      }
-    }).promise();
-  }
-};
-
-const putItem = async (Item) => {
-    return (await dynamodb.putItem({
-        TableName,
-        Item
-    }).promise());
-};
-// DynamoDB
-
-// Cost Explorer
-const CostExplorer = require("aws-sdk/clients/costexplorer");
-const costexplorer = new CostExplorer();
-
-const date = new Date();
-
-const SPLIT = "T";
-
-const getTimePeriod = (month, day) => {
-  return new Date(
-    date.getFullYear(),
-    month,
-    day
-  ).toISOString().split(SPLIT)[0];
-};
-
-const Start = getTimePeriod(date.getMonth()-1, 1);
-const End = getTimePeriod(date.getMonth() , 0);
-
-const Granularity = "MONTHLY";
-const Metrics = ["AmortizedCost"];
-
-const TimePeriod = {
-  Start,
-  End
-};
-
-const getCostAndUsage = async () => {
-  return await costexplorer.getCostAndUsage({
-    TimePeriod,
-    Granularity,
-    Metrics
-  }).promise();
-};
+const getCostAndUsage = require("./CostExplorer").getCostAndUsage;
+const clearItems = require("./DynamoDB").clearItems;
+const putItem = require("./DynamoDB").putItem;
 
 exports.handler = async (params, context) => {
   const data = await getCostAndUsage();
--- src/haasandmilan-api-search/index.js
+++ src/haasandmilan-api-search/index.js
@@ -4,13 +4,11 @@ const dynamodb = new DynamoDB();
 const TableName = "CostAndUsage";
 
 exports.handler = async (event) => {
-    const { Items } = await dynamodb.scan({
+    return (await dynamodb.scan({
         TableName,
         FilterExpression: "contains(AwsRequestId, :awsrequestid)", 
         ExpressionAttributeValues: {
             ":awsrequestid": {S: event.search}
         }
-    }).promise();
-    
-    return Items;
+    }).promise()).Items;
 };
