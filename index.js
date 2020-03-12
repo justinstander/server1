d61960a402167ab091544c137de4af614a2ffbd8
justinstander <justin@sundaygoods.com>
2020-03-12 07:41:54 -0400

    Code Cleanup

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 const _ = require("lodash");
+const fs = require("fs");
 
 const EMPTY_STRING = "";
+const FORWARD_SLASH = "/";
+const BODY_ENCODING = "text";
+const REQUEST_URI = "Records[0].cf.request.uri";
 
-const MODULE_NOT_FOUND = "MODULE_NOT_FOUND";
+const ERROR_ACCESS_DENIED_EXCEPTION = "AccessDeniedException";
+const ERROR_MODULE_NOT_FOUND = "MODULE_NOT_FOUND";
+
+const modules = (name) => {
+  switch(name) {
+    case "get-total-cost":
+      return require("./get-total-cost/");
+    case "put-total-cost":
+      return require("./put-total-cost/");
+    case "search":
+      return require("./search/");
+    default:
+      throw new Error("Module Not Found");
+  }
+};
 
 const getUri = (event) => {
   return _.get(
     event,
-    "Records[0].cf.request.uri",
+    REQUEST_URI,
     EMPTY_STRING
-  ).substring(1).split("/").filter((item) => {
+  ).substring(1).split(FORWARD_SLASH).filter((item) => {
     return item !== EMPTY_STRING;
   });
 };
 
-const response = (body, status, statusDescription, bodyEncoding = "text") => ({
+const response = (body, status, statusDescription, bodyEncoding = BODY_ENCODING) => ({
   body,
   bodyEncoding,
   status,
@@ -22,23 +40,24 @@ const response = (body, status, statusDescription, bodyEncoding = "text") => ({
 });
 
 const notFound = (body) => response(body, "404", "Not Found");
+
 const forbidden = (body) => response(body, "403", "Forbidden");
 
 const success = (body) => response(body, "200", "OK");
 
 const getError = (code) => {
   switch(code) {
-    case "AccessDeniedException":
+    case ERROR_ACCESS_DENIED_EXCEPTION:
       return forbidden("Access Denied");
-    case MODULE_NOT_FOUND:
+    case ERROR_MODULE_NOT_FOUND:
       return notFound("Module Not Found");
     default:
-      return notFound("Page Not Found");
+      return notFound("Not Found");
   }
 };
 
 const callMethod = async (method, event, context) => {
-  return success(await require(`./${method}/`).handler(
+  return success(await modules(method).handler(
     event,
     context
   ));
