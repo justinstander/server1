243828bae3bae5f3816eeaff7420ea7302170925
justinstander <justin@sundaygoods.com>
2020-03-28 13:53:51 -0400

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-zip -r "$1.zip" ./**/*.js ./*.js ./node_modules/lodash/** ./package.json && aws lambda update-function-code --function-name "$1" --zip-file "fileb://$1.zip" && rm "$1.zip"
+zip -r "$1.zip" ./**/*.js ./*.js ./node_modules/lodash/** ./package.json && \
+aws lambda update-function-code --function-name "$1" --zip-file "fileb://$1.zip" && \
+rm "$1.zip"
