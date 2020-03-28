9adeb365b3bfc6d7f8628a6eb8f7b41d8993bc60
justinstander <justin@sundaygoods.com>
2020-03-28 14:35:52 -0400

    creates new function version after updating the function

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 zip -r "$1.zip" ./**/*.js ./*.js ./node_modules/lodash/** ./package.json && \
 aws lambda update-function-code --function-name "$1" --zip-file "fileb://$1.zip" && \
-rm "$1.zip"
-
+aws lambda publish-version --function-name "$1" && \
+rm "$1.zip" && \
+./invalidate.sh "$2"
