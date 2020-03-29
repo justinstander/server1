d6b529889322d3e6f592a062f6e3e66d2bddd06c
justinstander <justin@sundaygoods.com>
2020-03-29 13:46:08 -0400

    tabs to spaces

--- errors/Http404.js
+++ errors/Http404.js
@@ -2,18 +2,18 @@ const status = "404";
 const description = "Not Found";
 
 module.exports = class Http405 extends Error {
-	static get STATUS() {
-    	return status;
-  	}
+  static get STATUS() {
+      return status;
+    }
 
-  	static get DESCRIPTION() {
-  		return description;
-  	}
+    static get DESCRIPTION() {
+      return description;
+    }
 
-	constructor(message, fileName=null, lineNumber=null) {
-		super(message, fileName, lineNumber);
+  constructor(message, fileName=null, lineNumber=null) {
+    super(message, fileName, lineNumber);
 
-		this.name = description;
-		this.code = status;
-	}
+    this.name = description;
+    this.code = status;
+  }
 };
--- errors/index.js
+++ errors/index.js
@@ -4,8 +4,8 @@ const Http405 = require("./Http405");
 const Http500 = require("./Http500");
 
 module.exports = {
-	Http403,
-	Http404, 
-	Http405,
-	Http500
+  Http403,
+  Http404, 
+  Http405,
+  Http500
 };
