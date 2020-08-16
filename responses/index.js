2e55b71985401b46fc7b71cdb547e2ce8ddbe76e
Justin Stander <justander@singlestop.org>
2020-08-16 16:49:33 -0400

    Added HTTP 410 gone error

--- responses/index.js
+++ responses/index.js
@@ -2,6 +2,7 @@ const Http200 = require("./Http200");
 const Http403 = require("./Http403");
 const Http404 = require("./Http404");
 const Http405 = require("./Http405");
+const Http410 = require("./Http410");
 const Http422 = require("./Http422");
 const Http500 = require("./Http500");
 
@@ -17,6 +18,7 @@ const error = {
   [Http405.STATUS]: Http405,
   [Http422.STATUS]: Http422,
   [Http500.STATUS]: Http500,
+  GoneException: Http410,
   MODULE_NOT_FOUND: Http404,
   ReferenceError: Http500,
   TypeError: Http500
